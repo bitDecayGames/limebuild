c428f7f68f54b3b28201a763d2652c7deb380942
mwingfield <mwingfie@gmail.com>
2020-08-30 02:21:20 -0600

    this time with actual dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -l
 
-echo "Hello world"
+echo "ls -la /var/haxelib"
+ls -la /var/haxelib
+# not sure why it is having me do this agian, but it is
+echo "haxelib setup /var/haxelib"
+haxelib setup /var/haxelib
+haxelib version
+haxelib path lime
+lime --version
